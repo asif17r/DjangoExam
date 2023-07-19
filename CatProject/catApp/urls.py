47a2e2741614ed2bc0702e31b7f88ceb53836dd7
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2023-07-19 09:45:45 +0600

    created urls.py

--- CatProject/catApp/urls.py
+++ CatProject/catApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    
+    path('', views.ListCat.as_view()), name='cat_list'),
+    path('<int:pk>/', views.DetailCat.as_view()), name='cat_detail'),
 ]
