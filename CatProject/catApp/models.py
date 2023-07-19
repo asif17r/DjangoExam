da24c12ff30eb92e2663d80b6fb875e4cd467c03
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2023-07-19 09:41:57 +0600

    created cat model

--- CatProject/catApp/models.py
+++ CatProject/catApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    discription = models.TextField()
+
+    def __str__(self):
+        return self.name
