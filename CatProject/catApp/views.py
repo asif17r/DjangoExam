b56186f4136423cd1a061ea88d3105609ca3e78a
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2023-07-19 09:44:17 +0600

    created views and serielizers

--- CatProject/catApp/views.py
+++ CatProject/catApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+class ListCat(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+class DetailCat(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+    
